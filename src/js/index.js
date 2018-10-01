096c68540e86cf97b2b61b75016aca33f959c542
Dev Mani Shukla <devmanishukla@Devs-MacBook-Pro.local>
2018-10-01 18:30:43 +0530

    Initial Commit

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements, renderLoader, clearLoader} from './views/base';
+
+const state = {};
+
+const controlSearch = async () => {
+
+    const query = searchView.getInput();
+
+    if (query) {
+
+        state.search = new Search(query);
+
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+
+        await state.search.getResults();
+
+        clearLoader();
+        searchView.renderResults(state.search.result);
+
+    }
+
+};
+
+elements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default class Search {
+
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = 'd3f1090e47a7a56108333ef33c3ed855';
+        const proxy = 'https://cors-anywhere.herokuapp.com/'
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+}
+
+
