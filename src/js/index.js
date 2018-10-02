1e5b89ea88995740ed3744664b65ca4eba040a9b
Dev Mani Shukla <devmanishukla@Devs-MacBook-Pro.local>
2018-10-02 07:35:56 +0530

    make changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import {elements, renderLoader, clearLoader} from './views/base';
+
 
 const state = {};
 
